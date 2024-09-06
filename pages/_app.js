4315b48d3d3de6f56a17525eadbdc90b9ef15724
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-09-06 23:32:13 +0100

    removing helmet provider in _app as it is redundent now we are using next/head

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,14 @@
 import '../styles/globals.css'; // Import global styles
 import Nav from '../src/components/common/Nav/Nav'; // Adjust paths based on your new structure
 import Footer from '../src/components/common/Footer/Footer'; 
-import { HelmetProvider } from 'react-helmet-async';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <HelmetProvider>
+    <>
       <Nav />
       <Component {...pageProps} />
       <Footer />
-    </HelmetProvider>
+    </>
   );
 }
 

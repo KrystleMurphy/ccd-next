6b17e0e35e38d08a7adc7e5dd4dfd8e6171651a7
Jem Lawton <jem@j-lawton.com>
2024-09-16 18:15:03 +0100

    rebase with main

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,7 +4,24 @@ import Team from './components/Team';
 import Advisors from './components/Advisors';
 import Partners from './components/Partners';
 import { fetchAirtableData } from '../data/AirtableData';
-
+export const metadata = {
+    title: 'About Us - CC Diagnostics',
+    description:
+      "Learn about CC Diagnostics' journey towards medical advancements in cervical cancer detection. Meet our dedicated team, and explore our advisors and partners who support our mission to improve women's health.",
+    keywords:
+      'about us, CC Diagnostics, medical advancements, cervical cancer detection, team, advisors, partners',
+    openGraph: {
+      url: 'https://www.cc-diagnostics.netlify.app/about',
+      title: 'About Us - CC Diagnostics',
+      description:
+        "Learn about CC Diagnostics' journey towards medical advancements in cervical cancer detection. Meet our dedicated team, and explore our advisors and partners who support our mission to improve women's health.",
+      images: [
+        {
+          url: 'https://www.cc-diagnostics.netlify.app/assets/logo-COHLTM4X.png',
+        },
+      ],
+    },
+  };
 export const revalidate = 60;
 // Server-side component to fetch data
 

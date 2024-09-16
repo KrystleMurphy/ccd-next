3de22b393cd1c2e671b22b8d64e91f75ca8e23b8
Jem Lawton <jem@j-lawton.com>
2024-09-16 18:37:04 +0100

    rebase with main

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,24 +5,6 @@ import teamPhoto from '@/src/assets/images/teamPhoto.jpg';
 import emailjs from '@emailjs/browser';
 import Image from 'next/image';
 
-
-export const metadata = {
-  title: "Contact Us - CC Diagnostics",
-  description:
-    "Get in touch with CC Diagnostics for any inquiries, support, or information. Fill out our contact form to reach our team and we will get back to you promptly.",
-  keywords: "contact, CC Diagnostics, customer support, inquiries, contact form",
-  openGraph: {
-    url: "https://www.cc-diagnostics.netlify.app/contact",
-    title: "Contact Us - CC Diagnostics",
-    description:
-      "Get in touch with CC Diagnostics for any inquiries, support, or information. Fill out our contact form to reach our team and we will get back to you promptly.",
-    images: [
-      {
-        url: "https://www.cc-diagnostics.netlify.app/assets/logo-COHLTM4X.png",
-      },
-    ],
-  },
-};
 export default function ContactPage() {
   const privacyPolicyUrl = '/pdf/PrivacyPolicy.pdf';
   const form = useRef();

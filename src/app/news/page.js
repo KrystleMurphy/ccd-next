4aebe0af68e479cfed9c0e1a9869db8421b709c3
Jem Lawton <jem@j-lawton.com>
2024-09-16 18:15:03 +0100

    rebase with main

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -1,7 +1,24 @@
 import FeaturedNews from './components/FeaturedNews';
 import NewsFeed from './components/NewsFeed';
 import { fetchAirtableData } from '../data/AirtableData';
-
+export const metadata = {
+  title: 'Latest News - CC Diagnostics',
+  description:
+    'Stay updated with the latest news, updates, and insights from CC Diagnostics. Explore our featured news and news feed to stay informed about our innovations and advancements in cervical cancer detection.',
+  keywords:
+    'news, CC Diagnostics, latest updates, cervical cancer, medical advancements',
+  openGraph: {
+    url: 'https://www.cc-diagnostics.netlify.app/news',
+    title: 'Latest News - CC Diagnostics',
+    description:
+      'Stay updated with the latest news, updates, and insights from CC Diagnostics. Explore our featured news and news feed to stay informed about our innovations and advancements in cervical cancer detection.',
+    images: [
+      {
+        url: 'https://www.cc-diagnostics.netlify.app/assets/logo-COHLTM4X.png',
+      },
+    ],
+  },
+};
 export const revalidate = 60;
 
 export const metadata = {

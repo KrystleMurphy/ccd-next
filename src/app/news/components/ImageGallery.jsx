c87d8410dd2f1500a0dca5318ce5b5bb1682b421
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-09-17 15:58:16 +0100

    fixing styling with image tag

--- src/app/news/components/ImageGallery.jsx
+++ src/app/news/components/ImageGallery.jsx
@@ -13,35 +13,34 @@ const ImageGallery = ({ images }) => {
     <figure className="mt-5 grid gap-4">
       {/* Main Image (Dynamically Selected) */}
       {images.length > 0 && (
-        <div className="relative w-full h-auto md:h-[480px]">
+        <div className="relative h-auto w-full max-w-full rounded-lg md:h-[480px]">
           <Image
             src={images[selectedImageIndex].url} // Use the selected image
-            alt={`Image ${selectedImageIndex + 1}`}
-            layout="responsive" // Automatically adjust based on the container's size
-            width={800} // Provide a width for responsive scaling
-            height={480} // Provide a height for responsive scaling
-            className="w-full h-auto max-w-full rounded-lg object-cover object-center py-8"
+            alt={`Selected Image ${selectedImageIndex + 1}`}
+            layout="fill" // Fill the container
+            objectFit="cover" // Maintain the aspect ratio and cover the area
+            className="rounded-lg"
           />
         </div>
       )}
 
       {/* Thumbnail Grid (Remaining Images) */}
       {images.length > 1 && (
-        <div className="grid grid-cols-8 gap-0 py-8">
+        <div className="grid grid-cols-8 gap-0">
           {images.map((image, index) => (
             <div
               key={index}
               onClick={() => handleThumbnailClick(index)}
-              className="relative w-full h-20 cursor-pointer"
+              className={`relative h-20 cursor-pointer overflow-hidden rounded-lg ${
+                index === selectedImageIndex ? 'ring-2 ring-ccDarkBlue' : ''
+              }`}
             >
               <Image
                 src={image.url}
                 alt={`Thumbnail ${index + 1}`}
-                layout="fill" // Use fill to keep the image within the div without distortion
-                objectFit="cover"
-                className={`object-cover object-center py-8 ${
-                  index === selectedImageIndex ? 'ring-2 ring-ccLightBlue' : ''
-                }`} // Highlight selected thumbnail
+                layout="fill" // Fill the container
+                objectFit="cover" // Maintain aspect ratio
+                className="object-cover"
               />
             </div>
           ))}

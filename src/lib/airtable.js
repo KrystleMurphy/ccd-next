4420a8983c2182ab732caabda3e3732afe5fd8ca
Jem Lawton <jem@j-lawton.com>
2024-09-17 12:56:39 +0100

    added Env's back into code

--- src/lib/airtable.js
+++ src/lib/airtable.js
@@ -1,8 +1,9 @@
 'use server';
 
 import Airtable from "airtable";
-const apiKey = 'patLsKooeR7KSUs3D.4fa1a4d866d0f3d253f1958657b31fc489380b9083a186782f7be0ff991b40f9';
-const baseId = 'appCfHeiwqePN27kL';
+console.log('AIRTABLE_BASE_ID:', process.env.AIRTABLE_BASE_ID);
+const apiKey = process.env.AIRTABLE_API_KEY;
+const baseId = process.env.AIRTABLE_BASE_ID;
 
 Airtable.configure({
     endpointUrl: 'https://api.airtable.com',

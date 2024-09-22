4f1ae575c7ef8644f9ef914d23b7b9bb0e9ef38b
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-09-22 19:30:43 +0100

    adding metadata.js for the about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,24 +9,24 @@ import { fetchAirtableData } from '../data/AirtableData';
 
 // Server-side component to fetch data
 
-export const metadata = {
-    title: 'About Us - CC Diagnostics',
-    description:
-      "Learn about CC Diagnostics' journey towards medical advancements in cervical cancer detection. Meet our dedicated team, and explore our advisors and partners who support our mission to improve women's health.",
-    keywords:
-      'about us, CC Diagnostics, medical advancements, cervical cancer detection, team, advisors, partners',
-    openGraph: {
-      url: 'https://www.ccdiagnostics.netlify.app/about',
-      title: 'About Us - CC Diagnostics',
-      description:
-        "Learn about CC Diagnostics' journey towards medical advancements in cervical cancer detection. Meet our dedicated team, and explore our advisors and partners who support our mission to improve women's health.",
-      images: [
-        {
-          url: 'https://www.ccdiagnostics.netlify.app/assets/teamPhoto.jpg',
-        },
-      ],
-    },
-  };
+// export const metadata = {
+//     title: 'About Us - CC Diagnostics',
+//     description:
+//       "Learn about CC Diagnostics' journey towards medical advancements in cervical cancer detection. Meet our dedicated team, and explore our advisors and partners who support our mission to improve women's health.",
+//     keywords:
+//       'about us, CC Diagnostics, medical advancements, cervical cancer detection, team, advisors, partners',
+//     openGraph: {
+//       url: 'https://www.ccdiagnostics.netlify.app/about',
+//       title: 'About Us - CC Diagnostics',
+//       description:
+//         "Learn about CC Diagnostics' journey towards medical advancements in cervical cancer detection. Meet our dedicated team, and explore our advisors and partners who support our mission to improve women's health.",
+//       images: [
+//         {
+//           url: 'https://www.ccdiagnostics.netlify.app/assets/teamPhoto.jpg',
+//         },
+//       ],
+//     },
+//   };
 
   
 export default async function Page() {

5be493ce65824814902b2c773c96de0dd66a49fb
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-09-29 10:15:01 +0100

    swapping landing page hero buttons over

--- src/app/home/Hero.jsx
+++ src/app/home/Hero.jsx
@@ -28,13 +28,13 @@ export default function Hero() {
               At CC Diagnostics, we harness the power of medical advancements to improve the detection of cervical cancer. Our PCR-based reagent kit provide accurate and timely results, ensuring early intervention and better outcomes for patients.
             </p>
             <div className="mt-10 flex items-center justify-left gap-x-6">
-            <Link href="/contact" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-ccDarkBlue shadow-sm hover:bg-ccLightBlue hover:text-ccDarkBlue 
+            <Link href="#stats" className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-ccDarkBlue shadow-sm hover:bg-ccLightBlue hover:text-ccDarkBlue 
                 hover:border-solid hover:border-ccLightBlue
                 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-ccDarkBlue">
-              Contact Us
+              Learn More
             </Link>
-              <a href="#stats" className="text-sm font-semibold leading-6 text-white hover:text-ccLightBlue">
-                learn more <span aria-hidden="true">→</span>
+              <a href="/contact" className="text-sm font-semibold leading-6 text-white hover:text-ccLightBlue">
+                Contact Us <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>

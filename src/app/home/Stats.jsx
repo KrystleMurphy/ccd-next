7f43e095ed9f1b23d38d2e0300af5ef6fa4ac67b
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-10-05 10:05:42 +0100

    css styling

--- src/app/home/Stats.jsx
+++ src/app/home/Stats.jsx
@@ -1,7 +1,7 @@
 const stats = [
-    { id: 1, name: 'Cervical cancer cases go undetected', value: '144,000' },
-    { id: 2, name: 'False positives lead to unnecessary treatment.', value: '1.4 million' },
-    { id: 3, name: 'Most common cancer in women', value: '4th' },
+    { id: 1, des: 'Cervical cancer cases go undetected', name: '144,000' },
+    { id: 2, des: 'False positives lead to unnecessary treatment.', name: '1.4 million' },
+    { id: 3, des: 'Most common cancer in women', name: '4th' },
   ]
   
   export default function Stats() {
@@ -12,7 +12,7 @@ const stats = [
             <h2 className="text-4xl font-bold tracking-tight text-ccDarkBlue sm:text-6xl">
               Detect Cancer Early with epigenetic DNA analysis
             </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-xl leading-8 text-gray-600">
               Our cutting-edge technology allows for the early detection of cervical cancer, providing patients with a higher chance of successful treatment. With our innovative approach, we are able to identify abnormalities at the earliest stage, ensuring timely intervention and improved outcomes.
             </p>
           </div>
@@ -23,9 +23,9 @@ const stats = [
             <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
               {stats.map((stat) => (
                 <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
-                  <dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
-                  <dd className="order-first text-3xl font-semibold tracking-tight text-ccDarkBlue sm:text-5xl">
-                    {stat.value}
+                  <dt className="text-base text-xl leading-relaxed text-gray-600">{stat.des}</dt>
+                  <dd className="order-first text-3xl leading-relaxed font-semibold tracking-tight text-ccDarkBlue sm:text-5xl">
+                    {stat.name}
                   </dd>
                 </div>
               ))}

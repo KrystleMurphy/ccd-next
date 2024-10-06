7f060423446ae1c3c7a5f31be2403005d91146f1
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-10-06 18:15:17 +0100

    softening the about hero colors section and adding px padding

--- src/app/about/components/Hero.jsx
+++ src/app/about/components/Hero.jsx
@@ -1,18 +1,18 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import teamPhoto from '@/src/assets/images/teamPhoto.jpg'; 
+import teamPhoto from '@/src/assets/images/teamPhoto.jpg';
 import ContactHeavy from '@/src/components/contact-buttons/ContactHeavy';
 export default function Hero() {
   return (
     <div className="bg-white">
-      <div className="relative isolate overflow-hidden bg-gradient-to-b from-ccLightBlue/20 pt-14">
+      <div className="relative isolate overflow-hidden bg-gradient-to-b from-ccLightBlue/10 pt-14">
         <div
           aria-hidden="true"
-          className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-ccLightBlue/10 ring-1 ring-ccLightBlue sm:-mr-80 lg:-mr-96"
+          className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-ccLightBlue/10 ring-1 ring-ccLightBlue/10 sm:-mr-80 lg:-mr-96"
         />
         <div className="mx-auto max-w-7xl px-6 py-12 md:py-16 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-            <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-ccDarkBlue sm:text-6xl lg:col-span-2 xl:col-auto">
+          <div className="px-12 mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+            <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-ccDarkBlue sm:text-6xl lg:col-span-2 xl:col-auto ">
               Our Journey Towards Medical Advancements
             </h1>
             <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
@@ -29,7 +29,7 @@ export default function Hero() {
             <Image
               alt="CC-Diagnostics team photo"
               src={teamPhoto}
-              className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+              className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2"
               width={600} // Add appropriate width and height based on the image's dimensions
               height={500}
               priority // This helps prioritize the loading of the image

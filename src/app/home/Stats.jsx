41374659f68c734f6af6d28773f9b048a9c3542c
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-10-06 14:02:19 +0100

    added px-6 lg:px-8 to elements without x padding, added custom mobile styl;ing to text for centered elements to display on the left for mobiule sizes

--- src/app/home/Stats.jsx
+++ src/app/home/Stats.jsx
@@ -7,19 +7,19 @@ const stats = [
   export default function Stats() {
     return (
       <>
-        <div id="stats" className="bg-white px-4 py-12 md:py-16 lg:px-6">
-          <div className="mx-auto max-w-2xl text-center">
+        <div id="stats" className="bg-white px-6 py-12 md:py-16 lg:px-8">
+          <div className="mx-auto max-w-2xl text-left sm:text-center">
             <h2 className="text-4xl font-bold tracking-tight text-ccDarkBlue sm:text-6xl">
               Detect Cancer Early with epigenetic DNA analysis
             </h2>
-            <p className="mt-6 text-xl leading-8 text-gray-600">
+            <p className="mt-6 text-xl leading-8 text-gray-600 text-left sm:text-center">
               Our cutting-edge technology allows for the early detection of cervical cancer, providing patients with a higher chance of successful treatment. With our innovative approach, we are able to identify abnormalities at the earliest stage, ensuring timely intervention and improved outcomes.
             </p>
           </div>
         </div>
   
         <div className="bg-white py-12 sm:py-16">
-          <div className="mx-auto max-w-7xl px-4 lg:px-6">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
               {stats.map((stat) => (
                 <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
